82a9c8eed4c697a33085ebb94344dda386e3adef
Jerem <addoteipappoe23@gmail.com>
2025-08-17 10:59:43 +0000

    updated entry point file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-const PORT =  process.env.PORT || 8000;
+const PORT =  process.env.PORT || 8080;
 
 
 
